5588f7abd8072d191de456fc0da94c29d0430f08
arafat <arafatomer66@gmail.com>
2020-02-09 18:39:58 +0600

    auth

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPage implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onLogin(){
+    this.auth.login();
   }
 
 }
